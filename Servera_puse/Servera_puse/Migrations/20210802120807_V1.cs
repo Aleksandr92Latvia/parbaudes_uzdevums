b522ef61fb0ee4caefc3d12f3711eb0f66695c00
Александр Фадеев <88024895+Aleksandr92Latvia@users.noreply.github.com>
2021-08-02 15:10:37 +0300

    Add files via upload

---
+++ Servera_puse/Servera_puse/Migrations/20210802120807_V1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Servera_puse.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Datnes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Datnes_nosaukums = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Datnes_konteinera_nosaukums = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Komentari = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datnes", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Datnes");
+        }
+    }
+}
