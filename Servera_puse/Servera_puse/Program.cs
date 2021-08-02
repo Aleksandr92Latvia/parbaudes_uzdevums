d03d733cb28ad4221cafa3349f72cb6363c06ac8
Александр Фадеев <88024895+Aleksandr92Latvia@users.noreply.github.com>
2021-08-02 15:00:23 +0300

    Add files via upload

--- Servera_puse/Servera_puse/Program.cs
+++ Servera_puse/Servera_puse/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Servera_puse
 {
@@ -22,5 +23,26 @@ namespace Servera_puse
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    class Datne
+    {
+        public int ID { get; set; }
+        public string Datnes_nosaukums { get; set; }
+        public string Datnes_konteinera_nosaukums { get; set; }
+        public string Komentari { get; set; }
+    }
+
+    class Konteksts : DbContext
+    {
+        public DbSet<Datne> Datnes { get; set; }
+        public Konteksts()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Uzdevuma_DB;Trusted_Connection=True;");
+        }
     }
 }
